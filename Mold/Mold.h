828a258502aef24d2cc70aecfc6b4fe76ec795ed
Matt Quiros <mattquiros@gmail.com>
2016-02-19 22:03:07 +0800

    Initial commit

---
+++ Mold/Mold.h
@@ -0,0 +1,19 @@
+//
+//  Mold.h
+//  Mold
+//
+//  Created by Matt Quiros on 19/02/2016.
+//  Copyright © 2016 Matt Quiros. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Mold.
+FOUNDATION_EXPORT double MoldVersionNumber;
+
+//! Project version string for Mold.
+FOUNDATION_EXPORT const unsigned char MoldVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Mold/PublicHeader.h>
+
+
