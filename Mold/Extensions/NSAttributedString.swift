f4f3633d7692b15623740aa7e3198f95d5030cb3
Matt Quiros <mattquiros@gmail.com>
2016-07-29 01:53:19 +0800

    Added NSAttributedString extension, UIButton functions

---
+++ Mold/Extensions/NSAttributedString.swift
@@ -0,0 +1,28 @@
+//
+//  NSAttributedString.swift
+//  Mold
+//
+//  Created by Matt Quiros on 28/07/2016.
+//  Copyright © 2016 Matt Quiros. All rights reserved.
+//
+
+import Foundation
+
+extension NSAttributedString {
+    
+    public convenience init(string: String, font: UIFont, textColor: UIColor) {
+        self.init(string: string, attributes: [
+            NSFontAttributeName : font,
+            NSForegroundColorAttributeName : textColor
+            ])
+    }
+    
+    public convenience init(attributedStrings: NSAttributedString ...) {
+        let finalString = NSMutableAttributedString()
+        for string in attributedStrings {
+            finalString.appendAttributedString(string)
+        }
+        self.init(attributedString: finalString)
+    }
+    
+}
--- Mold/Extensions/UIButton.swift
+++ Mold/Extensions/UIButton.swift
@@ -2,19 +2,17 @@
 //  UIButton.swift
 //  Mold
 //
-//  Created by Matt Quiros on 04/07/2016.
+//  Created by Matt Quiros on 28/07/2016.
 //  Copyright © 2016 Matt Quiros. All rights reserved.
 //
 
 import UIKit
 
-public extension UIButton {
+extension UIButton {
     
-    public func setAttributedTitle(string: NSAttributedString?, forStates states: UIControlState ...) {
-        for state in states {
-            self.setAttributedTitle(string, forState: state)
-        }
+    public func setAttributedTitle(string: String, font: UIFont, textColor: UIColor) {
+        self.setAttributedTitle(NSAttributedString(string: string, font: font, textColor: textColor), forState: .Normal)
+        self.setAttributedTitle(NSAttributedString(string: string, font: font, textColor: textColor.colorWithAlphaComponent(0.2)), forState: .Highlighted)
     }
     
 }
-
