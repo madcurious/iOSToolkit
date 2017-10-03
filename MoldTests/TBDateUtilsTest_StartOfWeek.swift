fe36dbeaf47ec1cd9ed50fcb8b49ce853a61880b
Matt Quiros <mattquiros@gmail.com>
2017-10-03 17:01:26 +0800

    Added test cases

--- MoldTests/TBDateUtilsTest_StartOfWeek.swift
+++ MoldTests/TBDateUtilsTest_StartOfWeek.swift
@@ -22,6 +22,30 @@ class TBDateUtilsTest_StartOfWeek: TBDateUtilsTest {
     
     // MARK: - Dates within the month
     
+    func test_dateIsSunday_firstWeekdayIsSunday_shouldSucceed() {
+        doTest(dateArgs: (15, 10, 2017), firstWeekday: 1, expected: (15, 10, 2017, 0, 0, 0))
+    }
+    
+    func test_dateIsSunday_firstWeekdayIsMonday_shouldSucceed() {
+        doTest(dateArgs: (15, 10, 2017), firstWeekday: 2, expected: (9, 10, 2017, 0, 0, 0))
+    }
+    
+    func test_dateIsSunday_firstWeekdayIsSaturday_shouldSucceed() {
+        doTest(dateArgs: (15, 10, 2017), firstWeekday: 7, expected: (14, 10, 2017, 0, 0, 0))
+    }
+    
+    func test_dateIsMonday_firstWeekdayIsSunday_shouldSucceed() {
+        doTest(dateArgs: (9, 10, 2017), firstWeekday: 1, expected: (8, 10, 2017, 0, 0, 0))
+    }
+    
+    func test_dateIsMonday_firstWeekdayIsMonday_shouldSucceed() {
+        doTest(dateArgs: (9, 10, 2017), firstWeekday: 2, expected: (9, 10, 2017, 0, 0, 0))
+    }
+    
+    func test_dateIsMonday_firstWeekdayIsSaturday_shouldSucceed() {
+        doTest(dateArgs: (9, 10, 2017), firstWeekday: 7, expected: (7, 10, 2017, 0, 0, 0))
+    }
+    
     func test_dateIsSaturday_firstWeekdayIsSunday_shouldSucceed() {
         doTest(dateArgs: (23, 9, 2017), firstWeekday: 1, expected: (17, 9, 2017, 0, 0, 0))
     }
@@ -60,18 +84,6 @@ class TBDateUtilsTest_StartOfWeek: TBDateUtilsTest {
         doTest(dateArgs: (2, 10, 2017), firstWeekday: 7, expected: (30, 9, 2017, 0, 0, 0))
     }
     
-    func test_dateIsSaturday_firstWeekdayIsSunday_isCrossingMonth_shouldSucceed() {
-        doTest(dateArgs: (3, 6, 2017), firstWeekday: 1, expected: (28, 5, 2017, 0, 0, 0))
-    }
-    
-    func test_dateIsSaturday_firstWeekdayIsMonday_isCrossingMonth_shouldSucceed() {
-        doTest(dateArgs: (3, 6, 2017), firstWeekday: 2, expected: (29, 5, 2017, 0, 0, 0))
-    }
-    
-    func test_dateIsSaturday_firstWeekdayIsSaturday_isCrossingmonth_shouldSucceed() {
-        doTest(dateArgs: (3, 6, 2017), firstWeekday: 7, expected: (3, 6, 2017, 0, 0, 0))
-    }
-    
     func test_dateIsThursday_firstWeekdayIsSunday_isCrossingMonth_shouldSucceed() {
         doTest(dateArgs: (1, 6, 2017), firstWeekday: 1, expected: (28, 5, 2017, 0, 0, 0))
     }
@@ -82,6 +94,18 @@ class TBDateUtilsTest_StartOfWeek: TBDateUtilsTest {
     
     func test_dateIsThursday_firstWeekdayIsSaturday_isCrossingMonth_shouldSucceed() {
         doTest(dateArgs: (1, 6, 2017), firstWeekday: 7, expected: (27, 5, 2017, 0, 0, 0))
+    }
+    
+    func test_dateIsSaturday_firstWeekdayIsSunday_isCrossingMonth_shouldSucceed() {
+        doTest(dateArgs: (3, 6, 2017), firstWeekday: 1, expected: (28, 5, 2017, 0, 0, 0))
+    }
+    
+    func test_dateIsSaturday_firstWeekdayIsMonday_isCrossingMonth_shouldSucceed() {
+        doTest(dateArgs: (3, 6, 2017), firstWeekday: 2, expected: (29, 5, 2017, 0, 0, 0))
+    }
+    
+    func test_dateIsSaturday_firstWeekdayIsSaturday_isCrossingmonth_shouldSucceed() {
+        doTest(dateArgs: (3, 6, 2017), firstWeekday: 7, expected: (3, 6, 2017, 0, 0, 0))
     }
     
 }
